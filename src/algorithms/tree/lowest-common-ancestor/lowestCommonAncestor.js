6b387a1392b44827220b0cf44a66a76a826282aa
orist22 <orist22@gmail.com>
2018-12-05 11:39:38 +0900

    [ADD] add directory and files for lowest common ancestor

---
+++ src/algorithms/tree/lowest-common-ancestor/lowestCommonAncestor.js
@@ -0,0 +1,20 @@
+import Queue from '../../../data-structures/queue/Queue';
+
+/**
+ * @typedef {Object} Callbacks
+ * @property {function(node: BinaryTreeNode, child: BinaryTreeNode): boolean} allowTraversal -
+ *   Determines whether DFS should traverse from the node to its child.
+ */
+
+/**
+ * @param {Callbacks} [callbacks]
+ * @returns {Callbacks}
+ */
+
+/**
+ * @param {BinaryTreeNode} rootNode
+ * @param {Callbacks} [originalCallbacks]
+ */
+export default function lca(rootNode, originalCallbacks) {
+
+}
