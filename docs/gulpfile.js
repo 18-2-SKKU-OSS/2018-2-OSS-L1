2e546d00ff7f37f27a25135ee43912b5988918aa
김윤지 <yunjiyunji1@naver.com>
2018-12-06 23:53:10 +0900

    [UPDATE] Update gulpfile.js to change css file path

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -4,6 +4,7 @@ var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
 var jade        = require('gulp-jade');
+var deploy      = require('gulp-gh-pages');
 
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
@@ -23,7 +24,6 @@ gulp.task('jekyll-build', function (done) {
 
 
 
-
 /**
  * Rebuild Jekyll & do page reload
  */
@@ -32,15 +32,13 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 });
 
 
-
-
 /**
  * Wait for jekyll-build, then launch the Server
  */
 gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
     browserSync({
         server: {
-            baseDir: '_site'
+            baseDir: './_site'
         },
         notify: false
     });
@@ -53,15 +51,15 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-    return gulp.src('assets/css/main.scss')
+    return gulp.src('./assets/css/main.scss')
         .pipe(sass({
             includePaths: ['css'],
             onError: browserSync.notify
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(gulp.dest('./_site/assets/css'))
         .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('assets/css'));
+        .pipe(gulp.dest('./assets/css'));
 });
 
 /*
@@ -69,9 +67,9 @@ gulp.task('sass', function () {
 */
 
 gulp.task('jade', function(){
-  return gulp.src('_jadefiles/*.jade')
+  return gulp.src('./_jadefiles/*.jade')
   .pipe(jade())
-  .pipe(gulp.dest('_includes'));
+  .pipe(gulp.dest('./_includes'));
 });
 
 
@@ -80,11 +78,11 @@ gulp.task('jade', function(){
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('assets/css/**', ['sass']);
-    gulp.watch('assets/js/**', ['jekyll-rebuild']);
-    gulp.watch(['index.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
-    gulp.watch('assets/js/**', ['jekyll-rebuild']);
-    gulp.watch('_jadefiles/*.jade', ['jade']);
+    gulp.watch('./assets/css/**', ['sass']);
+    gulp.watch('./assets/js/**', ['jekyll-rebuild']);
+    gulp.watch(['index.html', './_layouts/*.html', './_includes/*'], ['jekyll-rebuild']);
+    gulp.watch('./assets/js/**', ['jekyll-rebuild']);
+    gulp.watch('./_jadefiles/*.jade', ['jade']);
 });
 
 
