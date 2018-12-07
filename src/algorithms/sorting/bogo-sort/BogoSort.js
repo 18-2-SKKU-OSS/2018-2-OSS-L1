1a440cbe34743b6ce19c75fe1e60b2ae44695386
MinjaeLyou <lminjae0313@gmail.com>
2018-12-07 22:36:04 +0900

    [UPDATE] BogoSort.js
    Update bogosort algorithm to add comments.

--- src/algorithms/sorting/bogo-sort/BogoSort.js
+++ src/algorithms/sorting/bogo-sort/BogoSort.js
@@ -2,10 +2,12 @@ import Sort from '../Sort';
 
 export default class BogoSort extends Sort {
     sort(originalArray) {
+      // Clone original array to prevent its modification.
       let array = [...originalArray];
-  
-      // Go through all array elements...
+      // Flag that holds info about whether the array is sorted or not.
       let sorted = false;
+
+      // Do while until the array is sorted.
       while(sorted == false){
         array = shuffle(array);
         sorted = isSorted(array);
@@ -13,10 +15,12 @@ export default class BogoSort extends Sort {
   
       return array;
     }
+    // Shuffle the array randomly.
     shuffle(array) {
       for(let j, x, i = array.length; i; j = Math.floor(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x);
       return array;
     }
+    // Check whether the array is sorted or not.
     isSorted(array) {
       for(let i = 1; i < array.length; i++){
         if (array[i-1] > array[i]) { return false; }
