89d40dcff9e2b8e8e5ff95815fc006c632f133f5
Minki-kim95 <rlaalsrl7777@gmail.com>
2018-12-08 23:16:45 +0900

    [ADD] Set complexity constants of sleep sort

--- src/algorithms/sorting/sleep-sort/__test__/SleepSort.test.js
+++ src/algorithms/sorting/sleep-sort/__test__/SleepSort.test.js
@@ -7,4 +7,10 @@ import {
   SortTester,
 } from '../../SortTester';
 
+// Complexity constants.
+const SORTED_ARRAY_VISITING_COUNT = 50;
+const NOT_SORTED_ARRAY_VISITING_COUNT = 50;
+const REVERSE_SORTED_ARRAY_VISITING_COUNT = 50;
+const EQUAL_ARRAY_VISITING_COUNT = 50;
+
 
