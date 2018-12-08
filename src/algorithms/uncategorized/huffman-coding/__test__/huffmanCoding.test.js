bf1f78940b4b8eaa9dc63a5526d39888031a516c
김윤지 <yunjiyunji1@naver.com>
2018-12-08 23:57:01 +0900

    [Refactor] Update test code's console log part

--- src/algorithms/uncategorized/huffman-coding/__test__/huffmanCoding.test.js
+++ src/algorithms/uncategorized/huffman-coding/__test__/huffmanCoding.test.js
@@ -3,126 +3,131 @@ var Heap = function(fn) {
       return e;
     };
     this.items = [];
-  };
+};
   
-  Heap.prototype = {
+Heap.prototype = {
     swap: function(i, j) {
-      this.items[i] = [
+        this.items[i] = [
         this.items[j],
         this.items[j] = this.items[i]
-      ][0];
+        ][0];
     },
     bubble: function(index) {
-      var parent = ~~((index - 1) / 2);
-      if (this.item(parent) < this.item(index)) {
+        var parent = ~~((index - 1) / 2);
+        if (this.item(parent) < this.item(index)) {
         this.swap(index, parent);
         this.bubble(parent);
-      }
+        }
     },
     item: function(index) {
-      return this.fn(this.items[index]);
+        return this.fn(this.items[index]);
     },
     pop: function() {
-      return this.items.pop();
+        return this.items.pop();
     },
     sift: function(index, end) {
-      var child = index * 2 + 1;
-      if (child < end) {
+        var child = index * 2 + 1;
+        if (child < end) {
         if (child + 1 < end && this.item(child + 1) > this.item(child)) {
-          child++;
+            child++;
         }
         if (this.item(index) < this.item(child)) {
-          this.swap(index, child);
-          return this.sift(child, end);
+            this.swap(index, child);
+            return this.sift(child, end);
         }
-      }
+        }
     },
     push: function() {
-      var lastIndex = this.items.length;
-      for (var i = 0; i < arguments.length; i++) {
+        var lastIndex = this.items.length;
+        for (var i = 0; i < arguments.length; i++) {
         this.items.push(arguments[i]);
         this.bubble(lastIndex++);
-      }
+        }
     },
     get length() {
-      return this.items.length;
+        return this.items.length;
     }
-  };
+};
   
-  var Huffman = {
+var Huffman = {
     // encode function
     encode: function(data) {
-      var prob = {};
-      var tree = new Heap(function(e) {
+        var prob = {};
+        var tree = new Heap(function(e) {
         return e[0];
-      });
-      for (var i = 0; i < data.length; i++) {
+        });
+        for (var i = 0; i < data.length; i++) {
         if (prob.hasOwnProperty(data[i])) {
-          prob[data[i]]++;
+            prob[data[i]]++;
         } else {
-          prob[data[i]] = 1;
+            prob[data[i]] = 1;
         }
-      }
-      Object.keys(prob).sort(function(a, b) {
+        }
+        Object.keys(prob).sort(function(a, b) {
         return ~~(Math.random() * 2);
-      }).forEach(function(e) {
+        }).forEach(function(e) {
         tree.push([prob[e], e]);
-      });
-      while (tree.length > 1) {
+        });
+        while (tree.length > 1) {
         var first = tree.pop(),
             second = tree.pop();
         tree.push([first[0] + second[0], [first[1], second[1]]]);
-      }
-      var dict = {};
-      var recurse = function(root, string) {
-        if (root.constructor === Array) {
-          recurse(root[0], string + '0');
-          recurse(root[1], string + '1');
-        } else {
-          dict[root] = string;
         }
-      };
-      tree.items = tree.pop()[1];
-      recurse(tree.items, '');
-      var result = '';
-      for (var i = 0; i < data.length; i++) {
+        var dict = {};
+        var recurse = function(root, string) {
+        if (root.constructor === Array) {
+            recurse(root[0], string + '0');
+            recurse(root[1], string + '1');
+        } else {
+            dict[root] = string;
+        }
+        };
+        tree.items = tree.pop()[1];
+        recurse(tree.items, '');
+        var result = '';
+        for (var i = 0; i < data.length; i++) {
         result += dict[data.charAt(i)];
-      }
-      var header = Object.keys(dict).map(function(e) {
+        }
+        var header = Object.keys(dict).map(function(e) {
         return e.charCodeAt(0) + '|' + dict[e];
-      }).join('-') + '/';
-      return header + result;
+        }).join('-') + '/';
+        return header + result;
     },
+
     // decode function
     decode: function(string) {
-      string = string.split('/');
-      var data = string[1].split(''),
-          header = {};
-      string[0].split('-').forEach(function(e) {
+        string = string.split('/');
+        var data = string[1].split(''),
+            header = {};
+        string[0].split('-').forEach(function(e) {
         var values = e.split('|');
         header[values[1]] = String.fromCharCode(values[0]);
-      });
-      var result = '';
-      while (data.length) {
+        });
+        var result = '';
+        while (data.length) {
         var i = 0,
             cur = '';
         while (data.length) {
-          cur += data.shift();
-          if (header.hasOwnProperty(cur)) {
+            cur += data.shift();
+            if (header.hasOwnProperty(cur)) {
             result += header[cur];
             break;
-          }
+            }
         }
-      }
-      return result;
+        }
+        return result;
     }
-  };
+};
   
-  /**** test code ****/
-  // test encode
-  var enc = Huffman.encode('OSS1234L1OSSTEST');
-  console.log(enc);
-  
-  // test decode
-  var dec = Huffman.decode(enc);
-  console.log(dec);
+
+/**** test code ****/
+var test = 'OSS1234L1OSSTEST'
+console.log("1. test string = ",test);
+
+// test encode
+var enc = Huffman.encode(test);
+console.log("2. encoded string = ",enc);
+
+// test decode
+var dec = Huffman.decode(enc);
+console.log("3. decoded string = ",dec);
