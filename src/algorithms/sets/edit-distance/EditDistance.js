606c0b95c9062ab85c565adfb8e8b9b6be5f3479
MinjaeLyou <lminjae0313@gmail.com>
2018-12-10 10:00:05 +0900

    [FIX] EditDistance.js to fix indentation

--- src/algorithms/sets/edit-distance/EditDistance.js
+++ src/algorithms/sets/edit-distance/EditDistance.js
@@ -5,27 +5,23 @@
  */
 export default function editDistance(set1, set2) {
   let dp[][] = new int[m+1][n+1]; 
-       
-        for (let i=0; i<=m; i++) 
-        { 
-            for (let j=0; j<=n; j++) 
-            { 
-                if (i==0) 
-                    dp[i][j] = j;  
-                else if (j==0) 
-                    dp[i][j] = i; 
-                else if (set1.charAt(i-1) == set2.charAt(j-1)) 
-                    dp[i][j] = dp[i-1][j-1]; 
-       
-                else
-                    dp[i][j] = 1 + min(dp[i][j-1],  
-                                       dp[i-1][j],  
-                                       dp[i-1][j-1]); 
-            } 
-        } 
-   
-        return dp[m][n]; 
-  
+  for (let i=0; i<=m; i++) 
+    { 
+      for (let j=0; j<=n; j++) 
+      { 
+        if (i==0) 
+          dp[i][j] = j;  
+        else if (j==0) 
+          dp[i][j] = i; 
+        else if (set1.charAt(i-1) == set2.charAt(j-1)) 
+          dp[i][j] = dp[i-1][j-1]; 
+        else
+          dp[i][j] = 1 + min(dp[i][j-1],  
+                             dp[i-1][j],  
+                             dp[i-1][j-1]); 
+      } 
+    } 
+  return dp[m][n]; 
 }
 
 function min (x, y, z) {
