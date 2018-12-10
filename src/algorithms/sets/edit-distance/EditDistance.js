141ae0d004508bab50ea5c9d3a9e5444d65d26f8
MinjaeLyou <lminjae0313@gmail.com>
2018-12-10 09:43:16 +0900

    [UPDATE] EditDistance.js to add function

--- src/algorithms/sets/edit-distance/EditDistance.js
+++ src/algorithms/sets/edit-distance/EditDistance.js
@@ -1,8 +1,29 @@
 /**
  * @param {string[]} set1
+ * @param {string[]} set2
  * @return {string[]}
  */
-export default function editDistance(set1) {
-    
-  return editDistance;
+export default function editDistance(set1, set2) {
+  let dp[][] = new int[m+1][n+1]; 
+       
+        for (let i=0; i<=m; i++) 
+        { 
+            for (let j=0; j<=n; j++) 
+            { 
+                if (i==0) 
+                    dp[i][j] = j;  
+                else if (j==0) 
+                    dp[i][j] = i; 
+                else if (str1.charAt(i-1) == str2.charAt(j-1)) 
+                    dp[i][j] = dp[i-1][j-1]; 
+       
+                else
+                    dp[i][j] = 1 + min(dp[i][j-1],  
+                                       dp[i-1][j],  
+                                       dp[i-1][j-1]); 
+            } 
+        } 
+   
+        return dp[m][n]; 
+  
 }
